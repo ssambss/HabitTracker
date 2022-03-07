d24a416754922f4628d573f66b48794c73f13f62
ssambss <samppa.kemppainen@hotmail.com>
2022-03-07 18:40:40 +0200

    habit form and styling

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import datetime
-from flask import Flask, render_template, request
+from collections import defaultdict
+from flask import Flask, redirect, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-habits=["Test habit", "habit 2"]
+habits=["Test habit"]
+
+completions = defaultdict(list)
 
 #Enables the date_range function to be called from every template
 @app.context_processor
@@ -21,10 +24,19 @@ def index():
         selected_date = datetime.date.fromisoformat(date_str)
     else:
         selected_date = datetime.date.today()
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home", selected_date=selected_date)
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home", selected_date=selected_date, completions=completions[selected_date])
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
     return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
+
+@app.post("/complete")
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habit_name")
+    date = datetime.date.fromisoformat(date_string)
+    completions[date].append(habit)
+
+    return redirect(url_for("index", date=date_string))
